513d1c6828439e68afff0c33443ed0917a10bac6
Vafelny Toster <genjavinogradov@gmail.com>
2026-07-03 18:29:00 +0300

    Add processing circular dependency

--- Src/Container.cs
+++ Src/Container.cs
@@ -7,7 +7,7 @@ namespace Wevear;
 public sealed class Container : IContainer
 {
     private readonly ImmutableDictionary<Type, ServiceDescriptor> _descriptors;
-    private readonly ConcurrentDictionary<Type, Func<IScope, object>> _cachedActivators;
+    private readonly ConcurrentDictionary<Type, Func<IScope, HashSet<Type>, object>> _cachedActivators;
     private readonly Scope _rootScope;
 
     public Container(in IEnumerable<ServiceDescriptor> descriptors)
@@ -26,34 +26,48 @@ public sealed class Container : IContainer
 
     public ValueTask DisposeAsync() => _rootScope.DisposeAsync();
 
-    private Func<IScope, object> BuildActivation(Type service)
+    private Func<IScope, HashSet<Type>, object> BuildActivation(Type service)
     {
         if (!_descriptors.TryGetValue(service, out var descriptor))
             throw new InvalidOperationException($"Service: {service.Name} not found");
 
         if (descriptor is InstanceBasedServiceDescriptor instance)
-            return _ => instance.Instance;
+            return (_, _) => instance.Instance;
         if (descriptor is FactoryBasedServiceDescriptor factory)
-            return factory.Factory;
+            return (scope, _) => factory.Factory;
 
         TypeBasedServiceDescriptor typeBasedServiceDescriptor = (TypeBasedServiceDescriptor)descriptor;
         ConstructorInfo constructor = typeBasedServiceDescriptor.ImplementationType.GetConstructors(BindingFlags.Public | BindingFlags.Instance).Single();
         ParameterInfo[] parameters = constructor.GetParameters();
 
-        return scope =>
+        return (scope, resolvingTypes) =>
         {
             object[] createdParameters = new object[parameters.Length];
 
             for (int i = 0; i < parameters.Length; i++)
-                createdParameters[i] = CreateInstance(parameters[i].ParameterType, scope);
+                createdParameters[i] = CreateInstance(parameters[i].ParameterType, scope, resolvingTypes);
 
             return constructor.Invoke(createdParameters);
         };
     }
 
-    private object CreateInstance(in Type service, in IScope scope)
+    private object CreateInstance(in Type service, in IScope scope, HashSet<Type> resolvingTypes)
     {
-        return _cachedActivators.GetOrAdd(service, BuildActivation(service)).Invoke(scope);
+        if (!resolvingTypes.Add(service))
+        {
+            throw new InvalidOperationException(
+                $"Circular dependency detected! Cycle: {string.Join(" -> ", resolvingTypes)} -> {service.Name}");
+        }
+
+        try
+        {
+            var activator = _cachedActivators.GetOrAdd(service, BuildActivation(service));
+            return activator.Invoke(scope, resolvingTypes);
+        }
+        finally
+        {
+            resolvingTypes.Remove(service);
+        }
     }
 
     private ServiceDescriptor FindDescriptor(in Type serviceType)
@@ -73,20 +87,11 @@ public sealed class Container : IContainer
             _container = container;
             _scopedInstances = new();
             _disposables = new();
-
         }
 
         public object Resolve(in Type serviceType)
         {
-            ServiceDescriptor descriptor = _container.FindDescriptor(serviceType);
-
-            if (descriptor.LifeTime == LifeTime.Transient)
-                return CreateInstanceInternal(serviceType);
-
-            if (descriptor.LifeTime == LifeTime.Scoped || _container._rootScope == this)
-                return _scopedInstances.GetOrAdd(serviceType, CreateInstanceInternal(serviceType));
-            else
-                return _container._rootScope.Resolve(serviceType);
+            return ResolveInternal(serviceType, new HashSet<Type>());
         }
 
         public T Resolve<T>()
@@ -116,9 +121,22 @@ public sealed class Container : IContainer
             }
         }
 
-        private object CreateInstanceInternal(Type serviceType)
+        private object ResolveInternal(in Type serviceType, HashSet<Type> resolvingTypes)
         {
-            object service = _container.CreateInstance(serviceType, this);
+            ServiceDescriptor descriptor = _container.FindDescriptor(serviceType);
+
+            if (descriptor.LifeTime == LifeTime.Transient)
+                return CreateInstanceInternal(serviceType, resolvingTypes);
+
+            if (descriptor.LifeTime == LifeTime.Scoped || _container._rootScope == this)
+                return _scopedInstances.GetOrAdd(serviceType, t => CreateInstanceInternal(t, resolvingTypes));
+            else
+                return _container._rootScope.ResolveInternal(serviceType, resolvingTypes);
+        }
+
+        private object CreateInstanceInternal(Type serviceType, HashSet<Type> resolvingTypes)
+        {
+            object service = _container.CreateInstance(serviceType, this, resolvingTypes);
 
             if (service is IDisposable || service is IAsyncDisposable)
                 _disposables.Push(service);

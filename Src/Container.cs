b8fe1ba457a10faadab1c65b19acaae23cc1dcb0
Vafelny Toster <genjavinogradov@gmail.com>
2026-07-03 17:32:17 +0300

    Cached service

--- Src/Container.cs
+++ Src/Container.cs
@@ -1,14 +1,18 @@
-﻿using System.Reflection;
+﻿using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Reflection;
 
 namespace Wevear;
 
 public sealed class Container : IContainer
 {
-    private readonly Dictionary<Type, ServiceDescriptor> _descriptors;
+    private readonly ImmutableDictionary<Type, ServiceDescriptor> _descriptors;
+    private readonly ConcurrentDictionary<Type, Func<IScope, object>> _cachedActivators;
 
     public Container(in IEnumerable<ServiceDescriptor> descriptors)
     {
-        _descriptors = descriptors.ToDictionary(d => d.ServiceType);
+        _descriptors = descriptors.ToImmutableDictionary(d => d.ServiceType);
+        _cachedActivators = new();
     }
 
     public IScope CreateScope()
@@ -16,25 +20,34 @@ public sealed class Container : IContainer
         return new Scope(this);
     }
 
-    private object CreateInstance(in Type service, in IScope scope)
+    private Func<IScope, object> BuildActivation(Type service)
     {
         if (!_descriptors.TryGetValue(service, out var descriptor))
             throw new InvalidOperationException($"Service: {service.Name} not found");
 
         if (descriptor is InstanceBasedServiceDescriptor instance)
-            return instance.Instance;
+            return _ => instance.Instance;
         if (descriptor is FactoryBasedServiceDescriptor factory)
-            return factory.Factory.Invoke(scope);
+            return factory.Factory;
 
         TypeBasedServiceDescriptor typeBasedServiceDescriptor = (TypeBasedServiceDescriptor)descriptor;
         ConstructorInfo constructor = typeBasedServiceDescriptor.ImplementationType.GetConstructors(BindingFlags.Public | BindingFlags.Instance).Single();
         ParameterInfo[] parameters = constructor.GetParameters();
-        object[] createdParameters = new object[parameters.Length];
 
-        for (int i = 0; i < parameters.Length; i++)
-            createdParameters[i] = CreateInstance(parameters[i].ParameterType, scope);
+        return scope =>
+        {
+            object[] createdParameters = new object[parameters.Length];
 
-        return constructor.Invoke(createdParameters);
+            for (int i = 0; i < parameters.Length; i++)
+                createdParameters[i] = CreateInstance(parameters[i].ParameterType, scope);
+
+            return constructor.Invoke(createdParameters);
+        };
+    }
+
+    private object CreateInstance(in Type service, in IScope scope)
+    {
+        return _cachedActivators.GetOrAdd(service, BuildActivation(service)).Invoke(scope);
     }
 
     private T CreateInstance<T>(in IScope scope)
